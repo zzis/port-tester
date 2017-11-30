a137576dd6394334253fb50fbe9251fdf9b76e10
zhaizhe <zhaizhe@gelement.com>
2017-11-30 16:49:52 +0800

    + optomize message

--- client.js
+++ client.js
@@ -1,46 +1,73 @@
 const net = require('net');
 const config = require('./config');
+const colors = require('colors');
 
 var serverPort = config.servicePort;
 
-if(process.argv.length != 4){
-  console.log('usage: node socket_client ip port');
-  return;
+function main(){
+  if(process.argv.length != 4){
+    console.log('usage: node socket_client ip port');
+    return;
+  }
+  var ip = process.argv[2];
+  var portToTest = process.argv[3];
+  connectServerPort(ip, serverPort, portToTest);
 }
 
-var ip = process.argv[2];
-var portToTest = process.argv[3];
-
-testConnection(ip, serverPort);
-
-function testConnection(ip, port){
+function connectServerPort(ip, port, portToTest){
   var client = new net.Socket();
-
-  client.setTimeout(3000);
+  client.setTimeout(config.timeout);
   client.connect(port, ip, () => {
     client.write(portToTest);
-  })
+  });
   client.on('data', (data) => {
     if(data.toString('utf8') == "established"){
-      testConnection(ip, portToTest);
       client.destroy();
-    } else {
-      console.log('port ' + port + " connected");
-      client.destroy();
-    }
-  })
+      // for(var port of portsToTest){
+        connectTestPort(ip, portToTest);
+      // }
+    } 
+  });
   
   client.on('timeout', () => {
-    console.log('port', port, 'on host', ip, 'connect falied');
+    console.log('port', port.red, 'on host', ip.yellow, 'connection timeout');
     client.destroy();
-  })
+  });
   
   client.on('close', () => {
-    console.log('connection closed');
-  })
+    // console.log('connection closed');
+  });
   
   client.on('error', () => {
-    console.log('port', port, 'on host', ip, 'connect falied');
+    console.log('port', port.red, 'on host', ip.yellow, 'connect falied');
     client.destroy();
-  })
+  });
 }
+
+function connectTestPort(ip, port){
+  var client = new net.Socket();
+  client.setTimeout(config.timeout);
+  client.connect(port, ip, () => {
+    client.write('Test port');
+  });
+  client.on('data', (data) => {
+    console.log('port ' + port.green, 'on host', ip.green, "connected");
+    client.destroy();
+  });
+  
+  client.on('timeout', () => {
+    console.log('port', port.red, 'on host', ip.yellow, 'connection timeout');
+    client.destroy();
+  });
+  
+  client.on('close', () => {
+    // console.log('connection closed');
+  });
+  
+  client.on('error', () => {
+    console.log('port', port.red, 'on host', ip.yellow, 'connect falied');
+    client.destroy();
+  });
+}
+
+main();
--- server.js
+++ server.js
@@ -1,33 +1,46 @@
 const net = require('net');
 const config = require('./config');
+const colors = require('colors');
 
 var serverPort = config.servicePort;
 
 function start(){
-  net.createServer((socket) => {
-    socket.name = socket.remoteAddress + ":" + socket.remotePort;
-    socket.on('data', (data) => {
-      console.log("remote connected" + socket.name);
-      portToTest = data.toString('utf8');
-      listen(portToTest);
-      socket.write("established");
-    })
-  }).listen(serverPort);
-}
-
-function listen(port){
   var server = net.createServer((socket) => {
     socket.name = socket.remoteAddress + ":" + socket.remotePort;
     socket.on('data', (data) => {
-      console.log("remote connected" + socket.name);
-      console.log(data.toString('utf8'));
+      // console.log("remote connected" + socket.name);
+      portToTest = data.toString('utf8');
+      listenTestPort(portToTest);
+      socket.write("established");
+    });
+  });
+  server.listen(serverPort, () => {
+    console.log('Listening on', serverPort);
+  });
+  server.on('error', e => {
+    if(e.code === 'EADDRINNUSE'){
+      console.log('Port', serverPort, 'already in use, please change a port');
+    }
+    throw e;
+  });
+}
+
+function listenTestPort(port){
+  var server = net.createServer((socket) => {
+    socket.name = socket.remoteAddress + ":" + socket.remotePort;
+    socket.on('data', (data) => {
+      // console.log("remote connected" + socket.name);
+      console.log('remote', socket.name,'port', port.green, 'connected');
       socket.write("port" + socket.localPort + " connected");
       socket.destroy();
-    })
+      server.close();
+    });
   }).listen(port);
   server.on('error', (err) => {
     console.log(err);
-  })
+    server.close();
+    throw err;
+  });
 }
 
 start();
